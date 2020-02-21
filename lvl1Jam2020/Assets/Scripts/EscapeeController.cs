da89ae2b2a772a5effd9a960ed0128d6b13c201b
Artjoms-Jakovenko <artjoms.jakovenko@gmail.com>
2020-02-21 23:56:55 +0100

    Walking for escapee added

---
+++ lvl1Jam2020/Assets/Scripts/EscapeeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EscapeeController : MonoBehaviour
+{
+    public GameObject navMeshAgentChild;
+    public GameObject escapeeSprite;
+    NavMeshAgent navMeshAgent;
+    private void Start()
+    {
+        navMeshAgent = navMeshAgentChild.GetComponent<NavMeshAgent>();
+
+        navMeshAgent.SetDestination(new Vector3(5.0F, 8.0F));
+    }
+
+    private void Update()
+    {
+        escapeeSprite.transform.position = navMeshAgentChild.transform.position;
+    }
+
+}
