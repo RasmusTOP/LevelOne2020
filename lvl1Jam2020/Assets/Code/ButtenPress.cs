adc1572b8b45ab52f71ed2e80320bed05d29fba4
Rasmus Thorbjørn Overgaard Pedersen <rped@itu.dk>
2020-02-21 23:17:47 +0100

    SceneAndCode

---
+++ lvl1Jam2020/Assets/Code/ButtenPress.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtenPress : MonoBehaviour
+{
+    public Sprite pressedSpr, notePressedSpr;
+    private GameObject[] Doors;
+    public bool pressed;
+    private SpriteRenderer spriteR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Doors = GameObject.FindGameObjectsWithTag("Door");
+        spriteR = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(pressed == true)
+        {
+            spriteR.sprite = pressedSpr;
+        }else{
+            spriteR.sprite = notePressedSpr;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.isTrigger != true){
+            foreach (var door in Doors)
+            {
+                if(pressed == false)
+                {
+                    door.SetActive(false);
+                }else{
+                    door.SetActive(true);
+                }
+                
+            }
+            pressed = !pressed;
+        }
+    }
+}
---
+++ lvl1Jam2020/Assets/Code/Leaving.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaving : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int speed;
+    public bool isNotLit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isNotLit = true;
+        rb = GetComponent<Rigidbody2D>();
+        Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(),GameObject.Find("/Player").GetComponent<Collider2D>());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if(isNotLit == true)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            //rb.velocity = new Vector2(speed,rb.velocity.y);
+        }else
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+            //rb.velocity = new Vector2(0,0);
+        }
+        rb.velocity = new Vector2(speed,rb.velocity.y);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        switch (col.gameObject.tag )
+        {
+            case "Exit":
+            Debug.Log("you Lose");
+            break;
+
+            case "Light":
+            isNotLit = false;
+            break;
+        }
+
+        if(col.gameObject.tag == "Exit")
+        {
+            Debug.Log("you Lose");
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Light")
+        {
+           isNotLit = true;
+        }
+    }
+}
